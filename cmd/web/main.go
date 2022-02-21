846c8df7cd3a459b8438a53ca59c0e766f60a406
Carlos <sparkycj328@gmail.com>
2022-02-20 23:59:36 -0600

    Implemented static route/css

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,10 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Create a new file server to server static files and add route
+	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// start a new web server which listens on port 4000 and serves the servemux created above
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
