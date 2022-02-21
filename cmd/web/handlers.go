61b9d74ea0ce3b178582785274b4c1e06cd076c9
Carlos <sparkycj328@gmail.com>
2022-02-20 23:34:13 -0600

    Added additional template files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,8 +15,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// load in template files into a slice
+	files := []string{
+		"../../ui/html/home.page.tmpl",
+		"../../ui/html/base.layout.tmpl",
+		"../../ui/html/footer.partial.tmpl",
+	}
 	// parse template file
-	ts, err := template.ParseFiles("../../ui/html/home.page.tmpl")
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
